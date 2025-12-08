b714816d8b3d6f1505a22b02f0639fd5612ac6f3
Wellington Santos <wellington@kasta.io>
2025-12-08 16:20:55 -0300

    chore: bump package versions to 2.8.6-velto-main.1 across multiple packages

--- packages/app/src/version.ts
+++ packages/app/src/version.ts
@@ -1,13 +1,13 @@
 declare global {
   interface Window {
-    __ORDERLY_VERSION__?: {
-      [key: string]: string;
-    };
+      __ORDERLY_VERSION__?: {
+          [key: string]: string;
+      };
   }
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@veltodefi/react-app"] = "2.8.6-velto-main.0";
+  window.__ORDERLY_VERSION__["@veltodefi/react-app"] = "2.8.6-velto-main.1";
 }
 
-export default "2.8.6-velto-main.0";
+export default "2.8.6-velto-main.1";
