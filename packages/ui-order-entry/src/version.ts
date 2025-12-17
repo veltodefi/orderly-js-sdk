c68736e42d46a306aab50e1f282a12d1f397baaa
Aryella Lacerda <aryella@kasta.io>
2025-12-17 13:39:10 -0300

    chore: revert eslint

--- packages/ui-order-entry/src/version.ts
+++ packages/ui-order-entry/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
   window.__ORDERLY_VERSION__["@veltodefi/ui-order-entry"] =
-    "2.8.6-velto-main.5";
+    "2.8.6-velto-main.6";
 }
 
-export default "2.8.6-velto-main.5";
+export default "2.8.6-velto-main.6";
