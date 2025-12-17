0503c1545124d47c5ec0df60438379b0ab929e3e
Aryella Lacerda <aryella@kasta.io>
2025-12-17 13:41:53 -0300

    chore: update to 2.8.6-velto-main.7

--- packages/default-solana-adapter/src/version.ts
+++ packages/default-solana-adapter/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
   window.__ORDERLY_VERSION__["@veltodefi/default-solana-adapter"] =
-    "2.8.6-velto-main.6";
+    "2.8.6-velto-main.7";
 }
 
-export default "2.8.6-velto-main.6";
+export default "2.8.6-velto-main.7";
