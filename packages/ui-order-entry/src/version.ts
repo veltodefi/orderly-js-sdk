88ecfa2f0e95ce1d1ce5750f72446eed32e78dca
Aryella Lacerda <aryella@kasta.io>
2025-12-18 13:07:59 -0300

    chore: update to 2.8.6-velto-main.10

--- packages/ui-order-entry/src/version.ts
+++ packages/ui-order-entry/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
   window.__ORDERLY_VERSION__["@veltodefi/ui-order-entry"] =
-    "2.8.6-velto-main.9";
+    "2.8.6-velto-main.10";
 }
 
-export default "2.8.6-velto-main.9";
+export default "2.8.6-velto-main.10";
