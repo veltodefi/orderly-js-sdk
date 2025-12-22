2ac35aaced4f335a5c95455b1ae99d18f6c5d3f7
Aryella Lacerda <aryella@kasta.io>
2025-12-22 17:09:43 -0300

    refactor: export the hook useWalletConnectorPrivy

--- packages/trading-leaderboard/src/version.ts
+++ packages/trading-leaderboard/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
   window.__ORDERLY_VERSION__["@veltodefi/trading-leaderboard"] =
-    "2.8.6-velto-main.22";
+    "2.8.6-velto-main.23";
 }
 
-export default "2.8.6-velto-main.22";
+export default "2.8.6-velto-main.23";
--- packages/ui-chain-selector/src/version.ts
+++ packages/ui-chain-selector/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
   window.__ORDERLY_VERSION__["@veltodefi/ui-chain-selector"] =
-    "2.8.6-velto-main.22";
+    "2.8.6-velto-main.23";
 }
 
-export default "2.8.6-velto-main.22";
+export default "2.8.6-velto-main.23";
