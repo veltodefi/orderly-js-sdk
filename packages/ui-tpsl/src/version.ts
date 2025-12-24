0485288c28a27752d44ce51873ae6ace2dc151e3
Aryella Lacerda <aryella@kasta.io>
2025-12-24 08:44:46 -0300

    chore: update to 2.8.6-velto-main.24

--- packages/ui-tpsl/src/version.ts
+++ packages/ui-tpsl/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@veltodefi/ui-tpsl"] = "2.8.6-velto-main.23";
+  window.__ORDERLY_VERSION__["@veltodefi/ui-tpsl"] = "2.8.6-velto-main.24";
 }
 
-export default "2.8.6-velto-main.23";
+export default "2.8.6-velto-main.24";
--- packages/ui-transfer/src/version.ts
+++ packages/ui-transfer/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@veltodefi/ui-transfer"] = "2.8.6-velto-main.23";
+  window.__ORDERLY_VERSION__["@veltodefi/ui-transfer"] = "2.8.6-velto-main.24";
 }
 
-export default "2.8.6-velto-main.23";
+export default "2.8.6-velto-main.24";
