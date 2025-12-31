0bf74b78d87f7cb98c9b7aa78da2fbe23a4a72d3
Aryella Lacerda <aryella@kasta.io>
2025-12-31 11:18:57 -0300

    chore: bump version to 2.8.6-velto-dev.46

--- packages/affiliate/src/version.ts
+++ packages/affiliate/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@veltodefi/affiliate"] = "2.8.6-velto-dev.45";
+  window.__ORDERLY_VERSION__["@veltodefi/affiliate"] = "2.8.6-velto-dev.46";
 }
 
-export default "2.8.6-velto-dev.45";
+export default "2.8.6-velto-dev.46";
