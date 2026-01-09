87d5c5bfda1992c669420cb38f1d719791dc0ba7
Wellington Santos <wellington@kasta.io>
2026-01-09 14:38:43 -0300

    chore: bump package versions to 2.8.6-velto-dev.54 for multiple packages

--- packages/ui-tradingview/src/version.ts
+++ packages/ui-tradingview/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
   window.__ORDERLY_VERSION__["@veltodefi/ui-tradingview"] =
-    "2.8.6-velto-dev.53";
+    "2.8.6-velto-dev.54";
 }
 
-export default "2.8.6-velto-dev.53";
+export default "2.8.6-velto-dev.54";
