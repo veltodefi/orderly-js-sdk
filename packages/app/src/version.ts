530eb9be0d85039a55eb4130778b0d6d72daa832
Wellington Santos <wellington@kasta.io>
2026-01-09 12:59:25 -0300

    chore: bump package versions to 2.8.6-velto-dev.53 for multiple packages

--- packages/app/src/version.ts
+++ packages/app/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@veltodefi/react-app"] = "2.8.6-velto-dev.52";
+  window.__ORDERLY_VERSION__["@veltodefi/react-app"] = "2.8.6-velto-dev.53";
 }
 
-export default "2.8.6-velto-dev.52";
+export default "2.8.6-velto-dev.53";
--- packages/ui-chain-selector/src/version.ts
+++ packages/ui-chain-selector/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
   window.__ORDERLY_VERSION__["@veltodefi/ui-chain-selector"] =
-    "2.8.6-velto-dev.52";
+    "2.8.6-velto-dev.53";
 }
 
-export default "2.8.6-velto-dev.52";
+export default "2.8.6-velto-dev.53";
