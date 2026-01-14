f6f42008fe757da4e8547dfbdad9450377c2820d
Aryella Lacerda <aryella@kasta.io>
2026-01-14 11:00:46 -0300

    chore: bump version to 2.8.10-velto-main.1

--- packages/trading-rewards/src/version.ts
+++ packages/trading-rewards/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
   window.__ORDERLY_VERSION__["@veltodefi/trading-rewards"] =
-    "2.8.10-velto-dev.0";
+    "2.8.10-velto-main.1";
 }
 
-export default "2.8.10-velto-dev.0";
+export default "2.8.10-velto-main.1";
--- packages/wallet-connector-privy/src/version.ts
+++ packages/wallet-connector-privy/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
   window.__ORDERLY_VERSION__["@veltodefi/wallet-connector-privy"] =
-    "2.8.10-velto-dev.0";
+    "2.8.10-velto-main.1";
 }
 
-export default "2.8.10-velto-dev.0";
+export default "2.8.10-velto-main.1";
