8a713cccc7ad83e77097a392cdb85b3e0330d7ef
Wellington Santos <wellington@kasta.io>
2026-01-23 11:23:12 -0300

    chore: bump version to 2.8.10-velto-dev.9 for all packages

--- packages/web3-provider-ethers/src/version.ts
+++ packages/web3-provider-ethers/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
   window.__ORDERLY_VERSION__["@veltodefi/web3-provider-ethers"] =
-    "2.8.10-velto-dev.7";
+    "2.8.10-velto-dev.9";
 }
 
-export default "2.8.10-velto-dev.7";
+export default "2.8.10-velto-dev.9";
