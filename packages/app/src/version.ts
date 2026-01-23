8b9900223d0ad9cf8e525de6f78c4957f23ac789
Wellington Santos <wellington@kasta.io>
2026-01-23 08:21:27 -0300

    chore: bump version to 2.8.10-velto-dev.7 for multiple packages

--- packages/app/src/version.ts
+++ packages/app/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@veltodefi/react-app"] = "2.8.10-velto-dev.6";
+  window.__ORDERLY_VERSION__["@veltodefi/react-app"] = "2.8.10-velto-dev.7";
 }
 
-export default "2.8.10-velto-dev.6";
+export default "2.8.10-velto-dev.7";
--- packages/wallet-connector-privy/src/version.ts
+++ packages/wallet-connector-privy/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
   window.__ORDERLY_VERSION__["@veltodefi/wallet-connector-privy"] =
-    "2.8.10-velto-dev.6";
+    "2.8.10-velto-dev.7";
 }
 
-export default "2.8.10-velto-dev.6";
+export default "2.8.10-velto-dev.7";
