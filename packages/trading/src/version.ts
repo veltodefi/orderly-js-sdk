3f493d4795113dc421d99b5315d2b3ee7926c2f4
Wellington Santos <wellington@kasta.io>
2026-01-23 08:09:29 -0300

    chore: update package versions to 2.8.10-velto-dev.6 across multiple packages

--- packages/trading/src/version.ts
+++ packages/trading/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@veltodefi/trading"] = "2.8.10-velto-main.1";
+  window.__ORDERLY_VERSION__["@veltodefi/trading"] = "2.8.10-velto-dev.6";
 }
 
-export default "2.8.10-velto-main.1";
+export default "2.8.10-velto-dev.6";
