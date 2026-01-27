27205a51446025ce130f7e69d3e9ca91dcf3f58b
Wellington Santos <wellington@kasta.io>
2026-01-27 17:30:52 -0300

    chore: bump version to 2.8.10-velto-dev.15 for all packages

--- packages/ui-order-entry/src/version.ts
+++ packages/ui-order-entry/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
   window.__ORDERLY_VERSION__["@veltodefi/ui-order-entry"] =
-    "2.8.10-velto-dev.14";
+    "2.8.10-velto-dev.15";
 }
 
-export default "2.8.10-velto-dev.14";
+export default "2.8.10-velto-dev.15";
