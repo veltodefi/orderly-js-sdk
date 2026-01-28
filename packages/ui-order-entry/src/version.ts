08d5fe4c05a5ba2f33fe4116fa5a58e05984ab46
Wellington Santos <wellington@kasta.io>
2026-01-28 08:05:16 -0300

    chore: bump version to 2.8.10-velto-dev.16 for all packages

--- packages/ui-order-entry/src/version.ts
+++ packages/ui-order-entry/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
   window.__ORDERLY_VERSION__["@veltodefi/ui-order-entry"] =
-    "2.8.10-velto-dev.15";
+    "2.8.10-velto-dev.16";
 }
 
-export default "2.8.10-velto-dev.15";
+export default "2.8.10-velto-dev.16";
--- packages/ui-positions/src/version.ts
+++ packages/ui-positions/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@veltodefi/ui-positions"] = "2.8.10-velto-dev.15";
+  window.__ORDERLY_VERSION__["@veltodefi/ui-positions"] = "2.8.10-velto-dev.16";
 }
 
-export default "2.8.10-velto-dev.15";
+export default "2.8.10-velto-dev.16";
