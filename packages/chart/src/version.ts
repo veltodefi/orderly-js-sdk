e0743f91a395c04b01390f89add20d509c1a7080
Wellington Santos <wellington@kasta.io>
2026-01-30 15:12:43 -0300

    chore: update package versions from -dev.20 to -main.21 across all packages

--- packages/chart/src/version.ts
+++ packages/chart/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@veltodefi/chart"] = "2.8.10-velto-dev.20";
+  window.__ORDERLY_VERSION__["@veltodefi/chart"] = "2.8.10-velto-main.21";
 }
 
-export default "2.8.10-velto-dev.20";
+export default "2.8.10-velto-main.21";
--- packages/ui-notification/src/version.ts
+++ packages/ui-notification/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
   window.__ORDERLY_VERSION__["@veltodefi/ui-notification"] =
-    "2.8.10-velto-dev.20";
+    "2.8.10-velto-main.21";
 }
 
-export default "2.8.10-velto-dev.20";
+export default "2.8.10-velto-main.21";
