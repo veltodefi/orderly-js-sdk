21246d19f3744cf5f68b8e98a465dba29dc13fa5
Wellington Santos <wellington@kasta.io>
2026-01-30 08:15:20 -0300

    chore: update package versions to 2.8.10-velto-dev.20

--- packages/portfolio/src/version.ts
+++ packages/portfolio/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@veltodefi/portfolio"] = "2.8.10-velto-main.18";
+  window.__ORDERLY_VERSION__["@veltodefi/portfolio"] = "2.8.10-velto-dev.20";
 }
 
-export default "2.8.10-velto-main.18";
+export default "2.8.10-velto-dev.20";
--- packages/ui-order-entry/src/version.ts
+++ packages/ui-order-entry/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
   window.__ORDERLY_VERSION__["@veltodefi/ui-order-entry"] =
-    "2.8.10-velto-main.18";
+    "2.8.10-velto-dev.20";
 }
 
-export default "2.8.10-velto-main.18";
+export default "2.8.10-velto-dev.20";
