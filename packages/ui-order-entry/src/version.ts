2d2690f3d238e84d5545c4973190884c3237edde
Aryella Lacerda <aryella@kasta.io>
2026-02-02 14:32:05 -0300

    chore: bump version to 2.8.13-velto-dev.3

--- packages/ui-order-entry/src/version.ts
+++ packages/ui-order-entry/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
   window.__ORDERLY_VERSION__["@veltodefi/ui-order-entry"] =
-    "2.8.13-velto-dev.1";
+    "2.8.13-velto-dev.3";
 }
 
-export default "2.8.13-velto-dev.1";
+export default "2.8.13-velto-dev.3";
