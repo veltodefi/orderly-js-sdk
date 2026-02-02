143a05bf9f248f5e0fc13bdc7a84e03933907fd1
Aryella Lacerda <aryella@kasta.io>
2026-02-02 14:04:16 -0300

    chore: bump version to 2.8.13-velto-main.2

--- packages/default-solana-adapter/src/version.ts
+++ packages/default-solana-adapter/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
   window.__ORDERLY_VERSION__["@veltodefi/default-solana-adapter"] =
-    "2.8.13-velto-dev.1";
+    "2.8.13-velto-main.2";
 }
 
-export default "2.8.13-velto-dev.1";
+export default "2.8.13-velto-main.2";
--- packages/ui/src/version.ts
+++ packages/ui/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@veltodefi/ui"] = "2.8.13-velto-dev.1";
+  window.__ORDERLY_VERSION__["@veltodefi/ui"] = "2.8.13-velto-main.2";
 }
 
-export default "2.8.13-velto-dev.1";
+export default "2.8.13-velto-main.2";
