b6676769489b0d385e27234adbabebac88cb954f
Wellington Santos <wellington@kasta.io>
2026-02-04 13:56:41 -0300

    chore: update package versions to dev for multiple packages

--- packages/chart/src/version.ts
+++ packages/chart/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@veltodefi/chart"] = "2.8.13-velto-main.4";
+  window.__ORDERLY_VERSION__["@veltodefi/chart"] = "2.8.13-velto-dev.4";
 }
 
-export default "2.8.13-velto-main.4";
+export default "2.8.13-velto-dev.4";
