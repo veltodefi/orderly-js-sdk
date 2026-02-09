badeb6b92d771d78c69b2ac1547c0eb1c0f99765
Aryella Lacerda <aryella@kasta.io>
2026-02-09 16:34:50 -0300

    chore: bump version to 2.8.13-velto-dev.11

--- packages/trading-leaderboard/src/version.ts
+++ packages/trading-leaderboard/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
   window.__ORDERLY_VERSION__["@veltodefi/trading-leaderboard"] =
-    "2.8.13-velto-dev.10";
+    "2.8.13-velto-dev.11";
 }
 
-export default "2.8.13-velto-dev.10";
+export default "2.8.13-velto-dev.11";
--- packages/trading-points/src/version.ts
+++ packages/trading-points/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
   window.__ORDERLY_VERSION__["@veltodefi/trading-points"] =
-    "1.0.1-velto-dev.10";
+    "1.0.1-velto-dev.11";
 }
 
-export default "1.0.1-velto-dev.10";
+export default "1.0.1-velto-dev.11";
