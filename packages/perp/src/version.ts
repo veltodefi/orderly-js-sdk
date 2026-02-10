5bafd3af698ac895661a473c15b5d06fda3624f7
Aryella Lacerda <aryella@kasta.io>
2026-02-10 14:22:50 -0300

    chore: bump version to 2.8.13-velto-dev.13

--- packages/perp/src/version.ts
+++ packages/perp/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@veltodefi/perp"] = "4.8.13-velto-dev.12";
+  window.__ORDERLY_VERSION__["@veltodefi/perp"] = "4.8.13-velto-dev.13";
 }
 
-export default "4.8.13-velto-dev.12";
+export default "4.8.13-velto-dev.13";
--- packages/ui-connector/src/version.ts
+++ packages/ui-connector/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@veltodefi/ui-connector"] = "2.8.13-velto-dev.12";
+  window.__ORDERLY_VERSION__["@veltodefi/ui-connector"] = "2.8.13-velto-dev.13";
 }
 
-export default "2.8.13-velto-dev.12";
+export default "2.8.13-velto-dev.13";
