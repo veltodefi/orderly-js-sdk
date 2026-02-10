ea35270b2c40bda2d5f43677b2cbd934651a82fe
Aryella Lacerda <aryella@kasta.io>
2026-02-10 13:44:49 -0300

    chore: bump version to 2.8.13-velto-dev.12

--- packages/ui-chain-selector/src/version.ts
+++ packages/ui-chain-selector/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
   window.__ORDERLY_VERSION__["@veltodefi/ui-chain-selector"] =
-    "2.8.13-velto-dev.11";
+    "2.8.13-velto-dev.12";
 }
 
-export default "2.8.13-velto-dev.11";
+export default "2.8.13-velto-dev.12";
