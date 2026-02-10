7a54e55dcfd8350131f34c76999aefe8bbfd279e
Wellington Santos <wellington@kasta.io>
2026-02-10 16:22:15 -0300

    chore: update package versions to 2.8.13-velto-dev.14

--- packages/trading-points/src/version.ts
+++ packages/trading-points/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
   window.__ORDERLY_VERSION__["@veltodefi/trading-points"] =
-    "1.0.1-velto-main.6";
+    "1.0.1-velto-dev.14";
 }
 
-export default "1.0.1-velto-main.6";
+export default "1.0.1-velto-dev.14";
