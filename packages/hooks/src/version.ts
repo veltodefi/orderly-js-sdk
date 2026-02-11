d69e7d9ee29157b98b24aa4865c2ae410ade1ded
Wellington Santos <wellington@kasta.io>
2026-02-11 15:26:38 -0300

    chore: update package versions to 2.8.13-velto-dev.15

--- packages/hooks/src/version.ts
+++ packages/hooks/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@veltodefi/hooks"] = "2.8.13-velto-main.14";
+  window.__ORDERLY_VERSION__["@veltodefi/hooks"] = "2.8.13-velto-dev.15";
 }
 
-export default "2.8.13-velto-main.14";
+export default "2.8.13-velto-dev.15";
--- packages/ui-tpsl/src/version.ts
+++ packages/ui-tpsl/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@veltodefi/ui-tpsl"] = "2.8.13-velto-main.14";
+  window.__ORDERLY_VERSION__["@veltodefi/ui-tpsl"] = "2.8.13-velto-dev.15";
 }
 
-export default "2.8.13-velto-main.14";
+export default "2.8.13-velto-dev.15";
