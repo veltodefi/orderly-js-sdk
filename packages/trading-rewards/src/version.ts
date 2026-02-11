b7e57f437be1088ca6d28df4551040813be726de
Wellington Barea <wellington@kasta.io>
2026-02-11 15:26:14 -0300

    KPT-5441: Navbar update spacing (#25)
    * feat: update navbar spacing for improved layout
    
    * chore: update package versions to 2.8.13-velto-dev.14
    
    * chore: bump version to 2.8.13-velto-main.15 for all packages

--- packages/trading-rewards/src/version.ts
+++ packages/trading-rewards/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
   window.__ORDERLY_VERSION__["@veltodefi/trading-rewards"] =
-    "2.8.13-velto-main.14";
+    "2.8.13-velto-main.15";
 }
 
-export default "2.8.13-velto-main.14";
+export default "2.8.13-velto-main.15";
