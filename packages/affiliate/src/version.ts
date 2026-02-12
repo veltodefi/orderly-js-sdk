c2ddd00604646c11fc5fd1a403b07de7956965d3
Wellington Santos <wellington@kasta.io>
2026-02-12 10:56:52 -0300

    chore: bump version to 2.8.13-velto-dev.17 for multiple packages

--- packages/affiliate/src/version.ts
+++ packages/affiliate/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@veltodefi/affiliate"] = "2.8.13-velto-dev.16";
+  window.__ORDERLY_VERSION__["@veltodefi/affiliate"] = "2.8.13-velto-dev.17";
 }
 
-export default "2.8.13-velto-dev.16";
+export default "2.8.13-velto-dev.17";
--- packages/default-evm-adapter/src/version.ts
+++ packages/default-evm-adapter/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
   window.__ORDERLY_VERSION__["@veltodefi/default-evm-adapter"] =
-    "2.8.13-velto-dev.16";
+    "2.8.13-velto-dev.17";
 }
 
-export default "2.8.13-velto-dev.16";
+export default "2.8.13-velto-dev.17";
