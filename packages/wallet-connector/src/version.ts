c712c8a5bbbdf2010fa56a589f34936bc0663e23
Aryella Lacerda <aryella@kasta.io>
2026-02-19 10:05:33 -0300

    chore: bump version to 2.8.13-velto-dev.21

--- packages/wallet-connector/src/version.ts
+++ packages/wallet-connector/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
   window.__ORDERLY_VERSION__["@veltodefi/wallet-connector"] =
-    "2.8.13-velto-dev.16";
+    "2.8.13-velto-dev.21";
 }
 
-export default "2.8.13-velto-dev.16";
+export default "2.8.13-velto-dev.21";
