d1959dd835d04e95c8379f11122b3d3c316d8d4b
Aryella Lacerda <aryella.lacerda@gmail.com>
2026-02-19 16:30:16 -0300

    KPT-5454: Fix the Long/Short button colors (#28)
    * fix: add buy and sell active states to the main button
    
    * chore: bump version to 2.8.13-velto-dev.16
    
    * chore: bump version to 2.8.13-velto-dev.21
    
    * refactor: tweak disabled button states
    
    * chore: bump version to 2.8.13-velto-dev.22

--- packages/markets/src/version.ts
+++ packages/markets/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@veltodefi/markets"] = "2.8.13-velto-main.15";
+  window.__ORDERLY_VERSION__["@veltodefi/markets"] = "2.8.13-velto-dev.22";
 }
 
-export default "2.8.13-velto-main.15";
+export default "2.8.13-velto-dev.22";
--- packages/ui-order-entry/src/orderEntry.ui.tsx
+++ packages/ui-order-entry/src/orderEntry.ui.tsx
@@ -437,8 +437,8 @@ export const OrderEntry: React.FC<OrderEntryProps> = (props) => {
         <ThrottledButton
           fullWidth
           id={"order-entry-submit-button"}
-          // color={side === OrderSide.BUY ? "buy" : "sell"}
-          data-type={OrderSide.BUY}
+          data-type={side}
+          data-active={props.canTrade}
           variant="primary"
           className={cn(
             isMobile && freeCollateral <= 0
@@ -446,6 +446,8 @@ export const OrderEntry: React.FC<OrderEntryProps> = (props) => {
               : side === OrderSide.BUY
                 ? "orderly-order-entry-submit-button-buy"
                 : "orderly-order-entry-submit-button-sell",
+            !props.canTrade &&
+              "disabled:oui-bg-base-7 disabled:oui-text-base-contrast-36",
           )}
           onClick={() => {
             if (isMobile && freeCollateral <= 0) {
