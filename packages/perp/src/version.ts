d3782ee555a612539c5d9cc58c4db37062848850
Wellington Santos <wellington@kasta.io>
2026-02-23 08:29:35 -0300

    feat: update main version

--- packages/perp/src/version.ts
+++ packages/perp/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@veltodefi/perp"] = "4.8.13-velto-dev.23";
+  window.__ORDERLY_VERSION__["@veltodefi/perp"] = "4.8.13-velto-main.24";
 }
 
-export default "4.8.13-velto-dev.23";
+export default "4.8.13-velto-main.24";
--- packages/trading-rewards/src/version.ts
+++ packages/trading-rewards/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
   window.__ORDERLY_VERSION__["@veltodefi/trading-rewards"] =
-    "2.8.13-velto-dev.23";
+    "2.8.13-velto-main.24";
 }
 
-export default "2.8.13-velto-dev.23";
+export default "2.8.13-velto-main.24";
