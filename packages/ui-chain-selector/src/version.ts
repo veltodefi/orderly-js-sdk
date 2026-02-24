87c75e398b58d5a27006fdb6415da8d26c677bac
Wellington Santos <wellington@kasta.io>
2026-02-24 15:53:01 -0300

    chore: update package versions to 2.8.13-velto-dev.25

--- packages/ui-chain-selector/src/version.ts
+++ packages/ui-chain-selector/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
   window.__ORDERLY_VERSION__["@veltodefi/ui-chain-selector"] =
-    "2.8.13-velto-main.26";
+    "2.8.13-velto-dev.25";
 }
 
-export default "2.8.13-velto-main.26";
+export default "2.8.13-velto-dev.25";
