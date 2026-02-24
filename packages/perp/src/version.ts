b96e6da66190c9fe1c9a582e870e97d3b2e7a8e5
Wellington Santos <wellington@kasta.io>
2026-02-24 16:05:45 -0300

    chore: bump version to 2.8.13-velto-main.27 for all packages

--- packages/perp/src/version.ts
+++ packages/perp/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@veltodefi/perp"] = "4.8.13-velto-main.26";
+  window.__ORDERLY_VERSION__["@veltodefi/perp"] = "4.8.13-velto-main.27";
 }
 
-export default "4.8.13-velto-main.26";
+export default "4.8.13-velto-main.27";
