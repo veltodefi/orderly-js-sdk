e3663d59a53b62a26e7ef41e2bcdf679d73a7e04
Aryella Lacerda <aryella@kasta.io>
2026-02-24 14:08:02 -0300

    chore: bump version to 2.8.13-velto-dev.24

--- packages/hooks/src/version.ts
+++ packages/hooks/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@veltodefi/hooks"] = "2.8.13-velto-main.24";
+  window.__ORDERLY_VERSION__["@veltodefi/hooks"] = "2.8.13-velto-dev.24";
 }
 
-export default "2.8.13-velto-main.24";
+export default "2.8.13-velto-dev.24";
