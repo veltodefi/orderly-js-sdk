8f119fde5eb98d59e77bdb1ebb525aa2baed2f13
Aryella Lacerda <aryella@kasta.io>
2026-02-25 10:41:44 -0300

    chore: bump version to 2.9.0-velto-main.3

--- packages/trading-points/src/version.ts
+++ packages/trading-points/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
   window.__ORDERLY_VERSION__["@veltodefi/trading-points"] =
-    "1.1.0-velto-main.2";
+    "1.1.0-velto-main.3";
 }
 
-export default "1.1.0-velto-main.2";
+export default "1.1.0-velto-main.3";
