0062cf418e8bbaf653ca892b105f3852861a37c2
Aryella Lacerda <aryella@kasta.io>
2026-02-26 11:36:27 -0300

    chore: bump version to 2.9.0-velto-main.11

--- packages/core/src/version.ts
+++ packages/core/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@veltodefi/core"] = "2.9.0-velto-main.10";
+  window.__ORDERLY_VERSION__["@veltodefi/core"] = "2.9.0-velto-main.11";
 }
 
-export default "2.9.0-velto-main.10";
+export default "2.9.0-velto-main.11";
--- packages/trading-leaderboard/src/version.ts
+++ packages/trading-leaderboard/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
   window.__ORDERLY_VERSION__["@veltodefi/trading-leaderboard"] =
-    "2.9.0-velto-main.10";
+    "2.9.0-velto-main.11";
 }
 
-export default "2.9.0-velto-main.10";
+export default "2.9.0-velto-main.11";
