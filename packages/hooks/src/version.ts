a353767764ccb98fe995087f4b45d3ea60915d15
Aryella Lacerda <aryella@kasta.io>
2026-02-26 14:06:42 -0300

    chore: bump version to 2.9.0-velto-main.13

--- packages/hooks/src/version.ts
+++ packages/hooks/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@veltodefi/hooks"] = "2.9.0-velto-main.12";
+  window.__ORDERLY_VERSION__["@veltodefi/hooks"] = "2.9.0-velto-main.13";
 }
 
-export default "2.9.0-velto-main.12";
+export default "2.9.0-velto-main.13";
--- packages/web3-provider-ethers/src/version.ts
+++ packages/web3-provider-ethers/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
   window.__ORDERLY_VERSION__["@veltodefi/web3-provider-ethers"] =
-    "2.9.0-velto-main.12";
+    "2.9.0-velto-main.13";
 }
 
-export default "2.9.0-velto-main.12";
+export default "2.9.0-velto-main.13";
