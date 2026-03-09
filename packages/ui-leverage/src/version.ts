8690b6c0d95c2e41af7502c213cf41d3fcd87239
Wellington Santos <wellington@kasta.io>
2026-03-09 15:39:30 -0300

    chore: update package versions to 2.9.0-velto-dev.20 across all packages

--- packages/ui-leverage/src/version.ts
+++ packages/ui-leverage/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@veltodefi/ui-leverage"] = "2.9.0-velto-main.13";
+  window.__ORDERLY_VERSION__["@veltodefi/ui-leverage"] = "2.9.0-velto-dev.20";
 }
 
-export default "2.9.0-velto-main.13";
+export default "2.9.0-velto-dev.20";
