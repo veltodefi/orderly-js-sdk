03bfbdf9ea5711e787196cf822e022c6fb4dd966
Wellington Santos <wellington@kasta.io>
2026-03-16 12:03:31 -0300

    chore: update package versions to 2.9.0-velto-dev.25

--- packages/utils/src/version.ts
+++ packages/utils/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@veltodefi/utils"] = "2.9.0-velto-main.23";
+  window.__ORDERLY_VERSION__["@veltodefi/utils"] = "2.9.0-velto-dev.25";
 }
 
-export default "2.9.0-velto-main.23";
+export default "2.9.0-velto-dev.25";
--- packages/web3-provider-ethers/src/version.ts
+++ packages/web3-provider-ethers/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
   window.__ORDERLY_VERSION__["@veltodefi/web3-provider-ethers"] =
-    "2.9.0-velto-main.23";
+    "2.9.0-velto-dev.25";
 }
 
-export default "2.9.0-velto-main.23";
+export default "2.9.0-velto-dev.25";
