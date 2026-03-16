91125df3d56b39fa855769d8c409480687f4f88f
Wellington Santos <wellington@kasta.io>
2026-03-16 15:03:25 -0300

    chore: update package versions to 2.9.0-velto-main.26

--- packages/hooks/src/version.ts
+++ packages/hooks/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@veltodefi/hooks"] = "2.9.0-velto-dev.25";
+  window.__ORDERLY_VERSION__["@veltodefi/hooks"] = "2.9.0-velto-main.26";
 }
 
-export default "2.9.0-velto-dev.25";
+export default "2.9.0-velto-main.26";
--- packages/trading-points/src/version.ts
+++ packages/trading-points/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
   window.__ORDERLY_VERSION__["@veltodefi/trading-points"] =
-    "1.1.0-velto-dev.25";
+    "1.1.0-velto-main.26";
 }
 
-export default "1.1.0-velto-dev.25";
+export default "1.1.0-velto-main.26";
