9fed531591580f446053919ca028d9e6e7b57704
Aryella Lacerda <aryella@kasta.io>
2026-03-23 15:35:03 -0300

    chore: bump version to 2.11.1-velto-dev.6

--- packages/net/src/version.ts
+++ packages/net/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@veltodefi/net"] = "2.11.1-velto-dev.5";
+  window.__ORDERLY_VERSION__["@veltodefi/net"] = "2.11.1-velto-dev.6";
 }
 
-export default "2.11.1-velto-dev.5";
+export default "2.11.1-velto-dev.6";
