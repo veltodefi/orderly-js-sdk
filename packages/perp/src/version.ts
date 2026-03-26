916e1c27ae4d67c0ed899ad2c111d5aaba8c6628
Wellington Santos <wellington@kasta.io>
2026-03-26 16:38:42 -0300

    chore: update package versions to 2.11.1-velto-dev.23 across multiple packages

--- packages/perp/src/version.ts
+++ packages/perp/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@veltodefi/perp"] = "4.11.1-velto-main.16";
+  window.__ORDERLY_VERSION__["@veltodefi/perp"] = "4.11.1-velto-dev.23";
 }
 
-export default "4.11.1-velto-main.16";
+export default "4.11.1-velto-dev.23";
--- packages/portfolio/src/version.ts
+++ packages/portfolio/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@veltodefi/portfolio"] = "2.11.1-velto-main.16";
+  window.__ORDERLY_VERSION__["@veltodefi/portfolio"] = "2.11.1-velto-dev.23";
 }
 
-export default "2.11.1-velto-main.16";
+export default "2.11.1-velto-dev.23";
