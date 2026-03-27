1396acf54a26f516cd18c9e714d38063cbcd767b
Wellington Santos <wellington@kasta.io>
2026-03-27 13:47:11 -0300

    chore: update package versions to 2.11.1-velto-dev.33
    - Updated version for @veltodefi/i18n from 2.11.1-velto-dev.29 to 2.11.1-velto-dev.33
    - Updated version for @veltodefi/markets from 2.11.1-velto-dev.29 to 2.11.1-velto-dev.33
    - Updated version for @veltodefi/net from 2.11.1-velto-dev.29 to 2.11.1-velto-dev.33
    - Updated version for @veltodefi/perp from 4.11.1-velto-dev.29 to 4.11.1-velto-dev.33
    - Updated version for @veltodefi/portfolio from 2.11.1-velto-dev.29 to 2.11.1-velto-dev.33
    - Updated version for storybook-theme-tool from 2.11.1-velto-dev.29 to 2.11.1-velto-dev.33
    - Updated version for @veltodefi/trading-leaderboard from 2.11.1-velto-dev.29 to 2.11.1-velto-dev.33
    - Updated version for @veltodefi/trading-points from 1.3.1-velto-dev.29 to 1.3.1-velto-dev.33
    - Updated version for @veltodefi/trading-rewards from 2.11.1-velto-dev.29 to 2.11.1-velto-dev.33
    - Updated version for @veltodefi/trading from 2.11.1-velto-dev.29 to 2.11.1-velto-dev.33
    - Updated version for tsconfig from 0.14.1-velto-dev.29 to 0.14.1-velto-dev.33
    - Updated version for @veltodefi/types from 2.11.1-velto-dev.29 to 2.11.1-velto-dev.33
    - Updated version for @veltodefi/ui-chain-selector from 2.11.1-velto-dev.29 to 2.11.1-velto-dev.33
    - Updated version for @veltodefi/ui-connector from 2.11.1-velto-dev.29 to 2.11.1-velto-dev.33
    - Updated version for @veltodefi/ui-leverage from 2.11.1-velto-dev.29 to 2.11.1-velto-dev.33
    - Updated version for @veltodefi/ui-notification from 2.11.1-velto-dev.29 to 2.11.1-velto-dev.33
    - Updated version for @veltodefi/ui-order-entry from 2.11.1-velto-dev.29 to 2.11.1-velto-dev.33
    - Updated version for @veltodefi/ui-orders from 2.11.1-velto-dev.29 to 2.11.1-velto-dev.33
    - Updated version for @veltodefi/ui-positions from 2.11.1-velto-dev.29 to 2.11.1-velto-dev.33
    - Updated version for @veltodefi/ui-scaffold from 2.11.1-velto-dev.29 to 2.11.1-velto-dev.33
    - Updated version for @veltodefi/ui-share from 2.11.1-velto-dev.29 to 2.11.1-velto-dev.33
    - Updated version for @veltodefi/ui-tpsl from 2.11.1-velto-dev.29 to 2.11.1-velto-dev.33
    - Updated version for @veltodefi/ui-tradingview from 2.11.1-velto-dev.29 to 2.11.1-velto-dev.33
    - Updated version for @veltodefi/ui-transfer from 2.11.1-velto-dev.29 to 2.11.1-velto-dev.33
    - Updated version for @veltodefi/ui from 2.11.1-velto-dev.29 to 2.11.1-velto-dev.33
    - Updated version for @veltodefi/utils from 2.11.1-velto-dev.29 to 2.11.1-velto-dev.33
    - Updated version for @veltodefi/vaults from 2.11.1-velto-dev.29 to 2.11.1-velto-dev.33
    - Updated version for @veltodefi/wallet-connector-privy from 2.11.1-velto-dev.29 to 2.11.1-velto-dev.33
    - Updated version for @veltodefi/wallet-connector from 2.11.1-velto-dev.29 to 2.11.1-velto-dev.33
    - Updated version for @veltodefi/web3-provider-ethers from 2.11.1-velto-dev.29 to 2.11.1-velto-dev.33

--- packages/default-solana-adapter/src/version.ts
+++ packages/default-solana-adapter/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
   window.__ORDERLY_VERSION__["@veltodefi/default-solana-adapter"] =
-    "2.11.1-velto-dev.29";
+    "2.11.1-velto-dev.33";
 }
 
-export default "2.11.1-velto-dev.29";
+export default "2.11.1-velto-dev.33";
--- packages/vaults/src/version.ts
+++ packages/vaults/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@veltodefi/vaults"] = "2.11.1-velto-dev.29";
+  window.__ORDERLY_VERSION__["@veltodefi/vaults"] = "2.11.1-velto-dev.33";
 }
 
-export default "2.11.1-velto-dev.29";
+export default "2.11.1-velto-dev.33";
