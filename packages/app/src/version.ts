a4e57a3ede2ccc393c726242658fda547f93a39d
Wellington Santos <wellington@kasta.io>
2026-03-27 09:59:12 -0300

    chore: update package versions to 2.11.1-velto-dev.29 across multiple packages
    - Updated version in package.json and version.ts for the following packages:
      - @veltodefi/i18n
      - @veltodefi/markets
      - @veltodefi/net
      - @veltodefi/perp
      - @veltodefi/portfolio
      - storybook-theme-tool
      - @veltodefi/trading-leaderboard
      - @veltodefi/trading-points
      - @veltodefi/trading-rewards
      - @veltodefi/trading
      - tsconfig
      - @veltodefi/types
      - @veltodefi/ui-chain-selector
      - @veltodefi/ui-connector
      - @veltodefi/ui-leverage
      - @veltodefi/ui-notification
      - @veltodefi/ui-order-entry
      - @veltodefi/ui-orders
      - @veltodefi/ui-positions
      - @veltodefi/ui-scaffold
      - @veltodefi/ui-share
      - @veltodefi/ui-tpsl
      - @veltodefi/ui-tradingview
      - @veltodefi/ui-transfer
      - @veltodefi/ui
      - @veltodefi/utils
      - @veltodefi/vaults
      - @veltodefi/wallet-connector-privy
      - @veltodefi/wallet-connector
      - @veltodefi/web3-provider-ethers

--- packages/app/src/version.ts
+++ packages/app/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@veltodefi/react-app"] = "2.11.1-velto-dev.27";
+  window.__ORDERLY_VERSION__["@veltodefi/react-app"] = "2.11.1-velto-dev.29";
 }
 
-export default "2.11.1-velto-dev.27";
+export default "2.11.1-velto-dev.29";
--- packages/wallet-connector-privy/src/version.ts
+++ packages/wallet-connector-privy/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
   window.__ORDERLY_VERSION__["@veltodefi/wallet-connector-privy"] =
-    "2.11.1-velto-dev.27";
+    "2.11.1-velto-dev.29";
 }
 
-export default "2.11.1-velto-dev.27";
+export default "2.11.1-velto-dev.29";
