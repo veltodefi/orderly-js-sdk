9af94a65ea58eb93741553fe17bfe913324d14a9
Aryella Lacerda <aryella@kasta.io>
2026-04-27 15:21:14 -0300

    chore(release): publish 2.12.4-velto-main.2

--- packages/ui-connector/src/version.ts
+++ packages/ui-connector/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@veltodefi/ui-connector"] = "2.12.4-velto-dev.1";
+  window.__ORDERLY_VERSION__["@veltodefi/ui-connector"] = "2.12.4-velto-main.2";
 }
 
-export default "2.12.4-velto-dev.1";
+export default "2.12.4-velto-main.2";
