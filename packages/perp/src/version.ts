0bf3f9141846e9209b723d339b6a61c6801a32df
Aryella Lacerda <aryella@kasta.io>
2026-05-08 17:16:18 -0300

    chore(release): 2.12.4-velto-main.5

--- packages/perp/src/version.ts
+++ packages/perp/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@veltodefi/perp"] = "2.12.4-velto-main.3";
+  window.__ORDERLY_VERSION__["@veltodefi/perp"] = "2.12.4-velto-main.5";
 }
 
-export default "2.12.4-velto-main.3";
+export default "2.12.4-velto-main.5";
