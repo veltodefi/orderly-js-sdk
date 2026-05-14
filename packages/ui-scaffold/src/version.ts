c3c347bde8726a4d3aff6619c5aafa33aebd2e8c
Aryella Lacerda <aryella@kasta.io>
2026-05-14 15:03:04 -0300

    build: version bump

--- packages/ui-scaffold/src/version.ts
+++ packages/ui-scaffold/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@veltodefi/ui-scaffold"] = "3.0.4-velto-dev.0";
+  window.__ORDERLY_VERSION__["@veltodefi/ui-scaffold"] = "3.0.4-velto-dev.1";
 }
 
-export default "3.0.4-velto-dev.0";
+export default "3.0.4-velto-dev.1";
--- packages/wallet-connector/src/version.ts
+++ packages/wallet-connector/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
   window.__ORDERLY_VERSION__["@veltodefi/wallet-connector"] =
-    "3.0.4-velto-dev.0";
+    "3.0.4-velto-dev.1";
 }
 
-export default "3.0.4-velto-dev.0";
+export default "3.0.4-velto-dev.1";
