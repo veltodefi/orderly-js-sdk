96dcefe63d326478735155646de34ab8c75a3d29
Aryella Lacerda <aryella@kasta.io>
2026-05-22 16:41:49 -0300

    chore(release): bump all @veltodefi/* to 3.0.4-velto-dev.22

--- packages/cli/src/version.ts
+++ packages/cli/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@veltodefi/devkit"] = "1.0.4-velto-dev.21";
+  window.__ORDERLY_VERSION__["@veltodefi/devkit"] = "1.0.4-velto-dev.22";
 }
 
-export default "1.0.4-velto-dev.21";
+export default "1.0.4-velto-dev.22";
--- packages/ui-order-entry/src/version.ts
+++ packages/ui-order-entry/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
   window.__ORDERLY_VERSION__["@veltodefi/ui-order-entry"] =
-    "3.0.4-velto-dev.21";
+    "3.0.4-velto-dev.22";
 }
 
-export default "3.0.4-velto-dev.21";
+export default "3.0.4-velto-dev.22";
